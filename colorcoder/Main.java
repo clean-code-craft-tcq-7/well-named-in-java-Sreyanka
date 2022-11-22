8b2dd085e94a8a715596e8a10fce869482587631
Sreyanka <35264560+Sreyanka@users.noreply.github.com>
2022-11-22 23:17:35 +0530

    update error

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -1,7 +1,17 @@
+  
+package colorcoder;
+
+
+public class Main {
   public static void main(String[] args) {
         ColourPairsTest.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
         ColourPairsTest.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
     
         ColourPairsTest.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
         ColourPairsTest.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
+    
+    System.out.println("-- Reference Manual --");
+       ColorPair.printColorsAndPairNumber();
+       System.out.println("------");
     }
+}
